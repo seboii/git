063daf9496edff311aeae8ef1f8f8f7b9ad40540
sebo <shakho0303@gmail.com>
2025-03-14 19:52:38 +0300

    ilkgit

---
+++ kod.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_fib)
+    return fib_sequence
+
+# Kullanıcıdan dizinin kaç elemanını istediğini al
+num = int(input("Fibonacci dizisinin eleman sayısını girin: "))
+
+print("Fibonacci Dizisi:", fibonacci(num))
